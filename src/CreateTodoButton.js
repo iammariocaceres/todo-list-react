cc5419a654a59e754c3553478ac46e7c95d9f687
Mario Caceres <mcaceres@edarkstore.cl>
2022-01-30 19:08:13 -0300

    add events

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,17 @@ import React from "react";
 import './CreateTodoButton.css';
 
 function CreateTodoButton() {
+
+   const onClickButton = (msg) => {
+      alert(msg);
+   }
+
    return (
-      <button className="CreateTodoButton">+</button>
+      <button 
+         className="CreateTodoButton"
+         onClick={() => onClickButton("Aqui se debería mostrar el modal")}>
+         +
+      </button>
    )
 }
 
