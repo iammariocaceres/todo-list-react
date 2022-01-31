2a6d905962454f7cb465e9cc3de907b729ae8447
Mario Caceres <mcaceres@edarkstore.cl>
2022-01-30 21:46:36 -0300

    add events complete and delete task

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,18 +4,12 @@ import './TodoItem.css';
 
 function TodoItem(props) {
 
-   const onComplete = () => {
-      alert(`Ya completaste el TODO ${props.text}`)
-   }
-   const onDelete = () => {
-      alert(`Has eliminado el TODO ${props.text}`)
-   }
 
    return (
       <li className="TodoItem">
          <span
             className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-            onClick={onComplete}
+            onClick={props.onComplete}
          >
             √
          </span>
@@ -25,7 +19,7 @@ function TodoItem(props) {
          </p>
          <span
             className="Icon Icon-delete"
-            onClick={onDelete}>
+            onClick={props.onDelete}>
             X
          </span>
       </li>
